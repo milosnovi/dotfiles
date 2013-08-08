254549ed1c40ec3f8a341c0d6fb8475f3cf69a8c
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2013-08-08 20:28:36 +0200

    Show hidden files on Mac OSX

---
+++ osx/set-defaults.sh
@@ -0,0 +1,9 @@
+# Sets reasonable OS X defaults.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://raw.github.com/holman/dotfiles/master/osx/set-defaults.sh
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles YES
