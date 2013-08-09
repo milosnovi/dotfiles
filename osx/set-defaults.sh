2855fe2db312021144164476de62a0447a6545de
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2013-08-09 09:29:54 +0200

    Add Safari defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -6,4 +6,14 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Show hidden files
-defaults write com.apple.Finder AppleShowAllFiles YES
+# defaults write com.apple.Finder AppleShowAllFiles YES
+
+# Hide Safari's bookmark bar.
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
