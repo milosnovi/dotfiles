62d9508bc0ba9af95af7cb5b8e4761f1c8f95507
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2013-08-31 21:36:36 +0200

    Initial Vim setup
    Added plugins
     - Pathogen
     - NerdTree
     - Ctrl+P
     - Solarized

---
+++ bash/aliases/general.aliases.bash
@@ -0,0 +1,18 @@
+# https://github.com/revans/bash-it
+alias sl=ls
+alias ls='ls -G'        # Compact view, show colors
+alias la='ls -AF'       # Compact view, show hidden
+alias ll='ls -al'
+alias l='ls -a'
+alias l1='ls -1'
+
+alias ..='cd ..'         # Go up one directory
+alias ...='cd ../..'     # Go up two directories
+alias ....='cd ../../..' # Go up three directories
+alias -- -='cd -'        # Go back
+
+# Tree
+if [ ! -x "$(which tree 2>/dev/null)" ]
+then
+  alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+fi
